d3cdbfe12a726d3bb400270dde9ce1784b63c7a9
ezralfredo <ezralfredo@gmail.com>
2020-11-24 23:59:22 +0700

    Api test new feature: -Logout -Mengubah URI ke guestbook.test

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use GuzzleHttp\Client as GuzzleHttp;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -22,7 +23,7 @@ class LoginController extends Controller
     public function login(Request $request)
     {
 
-        $http = new \GuzzleHttp\Client;
+        $http = new GuzzleHttp;
 
         $user = [
             'email' => $request->email,
@@ -40,7 +41,7 @@ class LoginController extends Controller
                 if ($check->is_login == '0'){
                     if (Auth::attempt($user)){
                         $this->isLogin(Auth::id());
-                        $response = $http->post('http://127.0.0.1:8000/oauth/token',[
+                        $response = $http->post('http://guestbook.test/oauth/token',[
                             'form_params' => [
                                 'grant_type' => 'password',
                                 'client_id' => $this->client->id,
@@ -83,5 +84,48 @@ class LoginController extends Controller
             'is_login' => '1',
         ]);
     }
+
+    public function refresh(Request $request){
+        $this->validate($request,[
+            'refresh_token' => 'required'
+        ], [
+            'refresh_token' => 'refresh token is required',
+        ]);
+
+        $http = new GuzzleHttp;
+
+        $response = $http->post('http://guestbook.test/oauth/token',[
+            'form_params' => [
+                'grant_type' => 'refresh_token',
+                'client_id' => $this->client->id,
+                'client_secret' => $this->client->secret,
+                'refresh_token' => $request->refresh_token,
+                'scope' => '*',
+            ]
+        ]);
+
+        return json_decode((string) $response->getBody(), true);
+
+    }
+
+    public function logout(){
+        $user = Auth::user();
+        $accessToken = Auth::user()->token();
+        DB::table('oauth_refresh_tokens')
+            ->where('access_token_id', $accessToken->id)
+            ->update(['revoked' => true]);
+
+        $user->update([
+            'is_login' => '0',
+        ]);
+
+        $accessToken->revoke();
+
+        return response([
+            'message' => 'Logged out'
+        ]);
+    }
+
+
 
 }
